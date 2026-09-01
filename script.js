a7fb816b0e9e0886c5b48e20694309d0320f3b58
Boikobo Metsing <112819582+beekay12@users.noreply.github.com>
2026-09-01 12:45:33 +0200

    Add files via upload
    Updated index and styling and all new animations via javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.style.opacity = 1;
+        entry.target.style.transform = "translateY(0)";
+      }
+    });
+  });
+
+  document.querySelectorAll("section").forEach(section => {
+    section.style.opacity = 0;
+    section.style.transform = "translateY(40px)";
+    section.style.transition = "1s ease";
+    observer.observe(section);
+  });
